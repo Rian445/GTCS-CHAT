818770f2606778af97fa2b738f90c150f55f4506
Rian445 <rianislam445@gmail.com>
2025-03-17 07:19:02 +0600

    minor chnages

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 // Define the background message handler at the top level
